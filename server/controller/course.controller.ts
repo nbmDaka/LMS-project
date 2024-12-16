76f3260f81fca2736a27cccb7eb622c207451e67
Nurdaulet <nurdaulet.beketov.2005@gmail.com>
2024-12-16 15:29:00 +0500

    Get single and all courses without purchasing

--- server/controller/course.controller.ts
+++ server/controller/course.controller.ts
@@ -4,6 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import {createCourse} from "../services/course.service";
 import {courseModel} from "../models/course.model";
+import {redis} from "../utils/redis";
 
 
 // upload course
@@ -63,5 +64,66 @@ export const editCourse = CatchAsyncError(async(req: express.Request, res: expre
 
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500))
+    }
+});
+
+// get single course --- without purchasing
+
+export const getSingleCourse = CatchAsyncError(async(req: express.Request, res: express.Response, next: express.NextFunction) =>{
+    try {
+
+        const courseId = req.params.id
+
+        const isCacheExist = await redis.get(courseId)
+
+        if(isCacheExist) {
+            const course = JSON.parse(isCacheExist);
+            res.status(200).json({
+                success: true,
+                course,
+            })
+        }
+        else {
+        const course = await courseModel.findById(req.params.id).select(
+            "-courseData.questions");
+
+        await redis.set(courseId, JSON.stringify(course));
+
+        res.status(200).json({
+            success: true,
+            course,
+        })
+        }
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+// get all courses --- without purchasing
+export const getAllCourses = CatchAsyncError(async(req: express.Request, res: express.Response, next: express.NextFunction) =>{
+    try {
+
+        const isCacheExist = await redis.get("allCourses")
+
+        if(isCacheExist) {
+            const course = JSON.parse(isCacheExist);
+            res.status(200).json({
+                success: true,
+                course,
+            })
+        } else {
+
+            const courses = await courseModel.find().select(
+                "-courseData.questions");
+
+            await redis.set("allCourses", JSON.stringify(courses));
+
+            res.status(200).json({
+                success: true,
+                courses,
+            })
+        }
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
     }
 });
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import {editCourse, uploadCourse} from "../controller/course.controller";
+import {editCourse, getAllCourses, getSingleCourse, uploadCourse} from "../controller/course.controller";
 import {authorizeRoles, isAuthenticated} from "../middleware/auth";
 const courseRoutes = express.Router();
 
 courseRoutes.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse);
 courseRoutes.put("/edit-course/:id", isAuthenticated, authorizeRoles("admin"), editCourse);
+courseRoutes.get("/get-course/:id", isAuthenticated, authorizeRoles("admin"), getSingleCourse);
+courseRoutes.get("/get-courses", isAuthenticated, authorizeRoles("admin"), getAllCourses);
 
 export default courseRoutes;
